a2c7544db122514912482f5a72a7fc71319db733
nabi-tw <nabi.taniguchi@gmail.com>
2026-03-23 13:58:02 +0800

    Update cache version from v2 to v3
    更新したから

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "mnn-v2";
+const CACHE = "mnn-v3";
 const ASSETS = ["./", "./index.html", "./vocab.js", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", e => {
